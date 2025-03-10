b9c4f91301c9d1fea0377e900a2933cd694da313
Zoe <ddu_buk_e@naver.com>
2025-03-10 15:19:23 +0900

    feat: 리액트 라우터 돔 설치, 메인 화면 라우팅 확인

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,17 +1,25 @@
 import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
 
 const queryClient = new QueryClient(); // 일단 기본 옵션으로 설정. 추가 옵션 설정 후 파일 분리 및 import 해서 사용해도 됨
 // 쿼리 클라이언트 : 쿼리, 캐시, 쿼리 캐시를 조작하는 도구가 속함.
 // 대부분 이 도구를 직접 사용하는 대신 쿼리 클라이언트를 속성으로 쓰는 QueryClientProvider를 추가해서 사용한다.
 
 export function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+  ]);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <div className='flex h-screen items-center justify-center bg-gray-100'>
-        <h1 className='text-4xl font-bold text-blue-600'>에코니멀</h1>
-      </div>
+      <RouterProvider router={router} />
+      {/* ReactQueryDevtools : 탠스택쿼리 개발자도구입니다. npm run dev 했을 때 우측 하단에 있는 로고 누르면 뜹니다. */}
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
