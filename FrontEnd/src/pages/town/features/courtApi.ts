36833565215aadb2a6e49453c7d715224bac58d3
Zoe <ddu_buk_e@naver.com>
2025-03-11 17:46:23 +0900

    feat: 작업 중

--- FrontEnd/src/pages/town/features/courtApi.ts
+++ FrontEnd/src/pages/town/features/courtApi.ts
@@ -1,3 +1,13 @@
 // 법원 api 요청
+import { axiosInstance } from '@/api/axiosConfig';
 
-// 퀴즈는 언제 가져오는거지 -> 이벤트 발생했을 때 이미 와있는 건가
+const infraEventId = 1; // 법원에 해당하는 인프라 아이디로 변경해야함
+
+// 법원 이벤트 상세 조회(사용자가 클릭했을 때)
+const fetchCourtQuiz = async () => {
+  const response = await axiosInstance.get(`/towns/events/${infraEventId}`);
+
+  if (!response || !response.data) {
+    throw new Error(response.message || '법원 퀴즈를 가져오지 못했습니다.');
+  }
+};
