6a21e0af3ff0393c5a15862af4eb2f8826469183
Zoe <ddu_buk_e@naver.com>
2025-03-11 15:27:23 +0900

    chore: shadcn 설정

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,9 @@ export default defineConfig({
       typescript: true,
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
