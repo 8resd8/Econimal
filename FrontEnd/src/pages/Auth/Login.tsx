5de9922edc3c02d415a88c016541a88d81bac430
Dabeen <dabin12648@gmail.com>
2025-03-12 17:42:38 +0900

    faet: 로그인 화면 구성

---
+++ FrontEnd/src/pages/Auth/Login.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { useAuth } from "@/hooks/useAuth";
+import bgImage from "@/assets/auth_background.png"; // 배경 이미지
+import logoImage from "@/assets/logo.png"; // 로고 이미지
+
+const Login = () => {
+  const { login } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      await login(email, password);
+    } catch (error) {
+      alert("로그인 실패");
+    }
+  };
+
+  return (
+    <div
+      className="flex items-center justify-center min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="bg-opacity-90 p-8 rounded-lg text-center">
+        <img src={logoImage} alt="에코니멀 로고" className="mx-auto w-32 mb-4" />
+        <div className="flex flex-col space-y-3">
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="아이디"
+            className="p-3 border rounded-lg"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="비밀번호"
+            className="p-3 border rounded-lg"
+          />
+          <button
+            onClick={handleLogin}
+            className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600"
+          >
+            로그인
+          </button>
+          <div className="flex items-center justify-center space-x-2 text-sm">
+            <input type="checkbox" id="rememberMe" className="w-4 h-4" />
+            <label htmlFor="rememberMe">자동 로그인</label>
+          </div>
+          <a href="/signup" className="text-blue-400 text-sm">
+            아직 회원이 아니신가요? 회원가입 하러가기
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
--- FrontEnd/src/router.tsx
+++ FrontEnd/src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Town from './pages/town/Town';
+import Login from './pages/Auth/Login';
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: '/town',
     element: <Town />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
   },
 ]);
