83516f091b74e59294c75c4b1de9351c51a112b5
Zoe <ddu_buk_e@naver.com>
2025-03-12 15:56:59 +0900

    feat: 16:9 레이아웃 유지 컴포넌트

--- FrontEnd/src/components/AspectRatioContainer.tsx
+++ FrontEnd/src/components/AspectRatioContainer.tsx
@@ -8,6 +8,7 @@ const AspectRatioContainer = ({ children }: AspectRatioProps) => {
   return (
     <div className='fixed inset-0 flex items-center justify-center bg-black'>
       <div className='relative w-full max-w-screen-2xl aspect-video bg-white overflow-hidden'>
+        {/* <div className='relative w-full max-w-screen-2xl aspect-[20/9] bg-white overflow-hidden'> */}
         {children}
       </div>
     </div>
@@ -15,27 +16,3 @@ const AspectRatioContainer = ({ children }: AspectRatioProps) => {
 };
 
 export default AspectRatioContainer;
-
-// import { ReactNode } from 'react';
-
-// interface AspectRatioProps {
-//   children: ReactNode;
-// }
-
-// const AspectRatioContainer = ({ children }: AspectRatioProps) => {
-//   return (
-//     <div className='fixed inset-0 flex items-center justify-center bg-black'>
-//       <div
-//         className='relative bg-white w-screen h-screen'
-//         style={{
-//           maxWidth: 'calc(100vh * 16 / 9)',
-//           maxHeight: 'calc(100vw * 9 / 16)',
-//         }}
-//       >
-//         {children}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AspectRatioContainer;
