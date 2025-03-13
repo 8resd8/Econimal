554fd2e6dbf0c5fa658db35aaf930cb29343eba1
김현우 <dkanfjgwls@naver.com>
2025-03-13 17:42:25 +0900

    feat: 회원가입 요청

---
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/auth/dto/SignupRequest.java
@@ -0,0 +1,40 @@
+package com.ssafy.econimal.domain.auth.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import com.ssafy.econimal.global.common.enums.UserType;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record SignupRequest(
+	@NotBlank(message = "{required}")
+	@Email(message = "{valid}")
+	String email,
+
+	@NotBlank(message = "{required}")
+	@Size(min = 8, message = "{min.length}")
+	@Pattern(regexp = "^(?=.*[!@#$%^&*(),.?\":{}|<>]).*$", message = "{password}")
+	String password1,
+
+	@NotBlank(message = "{required}")
+	String password2,
+
+	@NotBlank(message = "{required}")
+	@Size(max = 255, message = "{max.length}")
+	String name,
+
+	@Size(max = 10, message = "{max.length}")
+	String nickname,
+
+	LocalDate birth,
+
+	@NotNull(message = "{required}")
+	UserType userType
+) {
+}
