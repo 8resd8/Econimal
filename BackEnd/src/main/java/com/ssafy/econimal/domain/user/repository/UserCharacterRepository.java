2c63ee1ac5a325eb79dab74e42bc20914d5bf3ab
김현우 <dkanfjgwls@naver.com>
2025-03-13 17:17:26 +0900

    feat: 기본 조회 메서드 구현

--- BackEnd/src/main/java/com/ssafy/econimal/domain/user/repository/UserCharacterRepository.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/user/repository/UserCharacterRepository.java
@@ -1,8 +1,13 @@
 package com.ssafy.econimal.domain.user.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ssafy.econimal.domain.user.entity.User;
 import com.ssafy.econimal.domain.user.entity.UserCharacter;
 
 public interface UserCharacterRepository extends JpaRepository<UserCharacter, Long> {
+
+	List<UserCharacter> findByUser(User user);
 }
