9ece42d358ae9ec7575cbdd8c55953c0421cdb47
김현우 <dkanfjgwls@naver.com>
2025-03-17 17:23:33 +0900

    feat: dto 빌더 적용

--- BackEnd/src/main/java/com/ssafy/econimal/domain/character/dto/UserCharacterMainDto.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/character/dto/UserCharacterMainDto.java
@@ -2,10 +2,15 @@ package com.ssafy.econimal.domain.character.dto;
 
 import com.ssafy.econimal.global.common.enums.ExpressionType;
 
+import lombok.Builder;
+
 public record UserCharacterMainDto(
 	Integer level,
 	Integer exp,
 	Long coin,
 	ExpressionType expression
 ) {
+	@Builder
+	public UserCharacterMainDto {
+	}
 }
