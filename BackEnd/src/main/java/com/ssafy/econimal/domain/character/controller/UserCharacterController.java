ef91b6b4b1ecf3ae057881381a857d661f9c5add
김현우 <dkanfjgwls@naver.com>
2025-03-17 16:49:30 +0900

    feat: 메인 상태변경 컨트롤러

--- BackEnd/src/main/java/com/ssafy/econimal/domain/character/controller/UserCharacterController.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/character/controller/UserCharacterController.java
@@ -1,8 +1,11 @@
 package com.ssafy.econimal.domain.character.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.econimal.domain.character.dto.UserCharacterDetailResponse;
@@ -26,7 +29,14 @@ public class UserCharacterController {
 	}
 
 	@GetMapping("/{userCharacterId}")
-	public UserCharacterDetailResponse getUserCharacterDetail(@Login User user, @PathVariable("userCharacterId") Long userCharacterId) {
+	public UserCharacterDetailResponse getUserCharacterDetail(@Login User user,
+		@PathVariable("userCharacterId") Long userCharacterId) {
 		return userCharacterService.getUserCharacterDetail(user, userCharacterId);
+	}
+
+	@PatchMapping("/main/{userCharacterId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void patchUserCharacterMain(@Login User user, @PathVariable("userCharacterId") Long userCharacterId) {
+		userCharacterService.updateUserCharacterMain(user, userCharacterId);
 	}
 }
