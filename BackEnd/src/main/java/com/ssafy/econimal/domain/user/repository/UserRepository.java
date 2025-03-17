8a85f2f68a3f855751d4a9f7eae7cae50e462ac5
김현우 <dkanfjgwls@naver.com>
2025-03-17 12:35:11 +0900

    feat: 유저정보 선택해서 가져오는 쿼리 작성

--- BackEnd/src/main/java/com/ssafy/econimal/domain/user/repository/UserRepository.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/user/repository/UserRepository.java
@@ -3,10 +3,16 @@ package com.ssafy.econimal.domain.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.ssafy.econimal.domain.user.dto.UserInfoDto;
 import com.ssafy.econimal.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByEmail(String email);
+
+	@Query("select new com.ssafy.econimal.domain.user.dto.UserInfoDto(c.email, c.name, c.nickname, c.birth, c.coin, c.role, c.lastLoginAt, c.town.name) FROM User c WHERE c.id = :userId")
+	UserInfoDto findUserInfoById(@Param("userId") Long userId);
 }
