5297922751c731c176a7490edd2ffd967fbbc1b7
주재완 <jjw05015@naver.com>
2025-03-18 09:50:44 +0900

    feat: 마을 이름 수정 API 작성

--- BackEnd/src/main/java/com/ssafy/econimal/domain/town/repository/TownRepository.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/town/repository/TownRepository.java
@@ -1,8 +1,12 @@
 package com.ssafy.econimal.domain.town.repository;
 
+import com.ssafy.econimal.domain.town.dto.TownNameUpdateDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.econimal.domain.town.entity.Town;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TownRepository extends JpaRepository<Town, Long> {
+    @Query("UPDATE Town t SET t.name = :townName WHERE t.id = :townId")
+    int updateTownName(TownNameUpdateDto townNameUpdateDto);
 }
