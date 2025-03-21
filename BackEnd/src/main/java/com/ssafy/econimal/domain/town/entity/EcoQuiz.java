9e7913c9288ed913c1bbc755559b95f23d4f89d5
주재완 <jjw05015@naver.com>
2025-03-21 16:09:20 +0900

    feat: 퀴즈, 인프라, 인프라이벤트 빌더 추가

--- BackEnd/src/main/java/com/ssafy/econimal/domain/town/entity/EcoQuiz.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/town/entity/EcoQuiz.java
@@ -13,6 +13,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -33,4 +34,10 @@ public class EcoQuiz extends BaseTimeEntity {
 
 	@Column(name = "description", nullable = false, columnDefinition = "TEXT")
 	private String description;
+
+	@Builder
+	public EcoQuiz(Facility facility, String description) {
+		this.facility = facility;
+		this.description = description;
+	}
 }
