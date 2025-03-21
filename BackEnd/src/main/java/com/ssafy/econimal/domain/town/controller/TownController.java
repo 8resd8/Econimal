729d47092a281f303a9bd52ef1ee2822adf02d46
주재완 <jjw05015@naver.com>
2025-03-21 09:19:02 +0900

    feat: 마을 상황 조회 API 작성
    - 이벤트 스케줄링은 미반영

--- BackEnd/src/main/java/com/ssafy/econimal/domain/town/controller/TownController.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/town/controller/TownController.java
@@ -1,16 +1,16 @@
 package com.ssafy.econimal.domain.town.controller;
 
 import com.ssafy.econimal.domain.town.dto.TownNameUpdateRequest;
+import com.ssafy.econimal.domain.town.dto.TownStatusResponse;
 import com.ssafy.econimal.domain.town.service.TownService;
 import com.ssafy.econimal.domain.user.entity.User;
 import com.ssafy.econimal.global.annotation.Login;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/towns")
@@ -23,5 +23,10 @@ public class TownController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateTownName(@Login User user, TownNameUpdateRequest townNameUpdateRequest) {
         townService.updateTownName(user, townNameUpdateRequest);
+    }
+
+    @GetMapping("/events/{townId}")
+    public TownStatusResponse getTownStatus(@Login User user) {
+        return townService.getTownStatus(user);
     }
 }
