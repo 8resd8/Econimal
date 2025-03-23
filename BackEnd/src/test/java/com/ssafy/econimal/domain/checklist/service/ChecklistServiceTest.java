8bf4f03655f3889ec40c632f64bea766a274016c
윤윤호 <yunho_yun@naver.com>
2025-03-23 23:26:16 +0900

    test: 커스텀 체크리스트 생성 테스트 코드 작성

--- BackEnd/src/test/java/com/ssafy/econimal/domain/checklist/service/ChecklistServiceTest.java
+++ BackEnd/src/test/java/com/ssafy/econimal/domain/checklist/service/ChecklistServiceTest.java
@@ -2,12 +2,18 @@ package com.ssafy.econimal.domain.checklist.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
+import com.ssafy.econimal.domain.checklist.dto.CustomChecklistRequest;
 import com.ssafy.econimal.domain.checklist.entity.Checklist;
+import com.ssafy.econimal.domain.checklist.util.CustomChecklistUtil;
 import com.ssafy.econimal.domain.data.TestEntityHelper;
 import com.ssafy.econimal.domain.town.entity.Town;
 import com.ssafy.econimal.domain.user.entity.User;
@@ -27,10 +33,15 @@ public class ChecklistServiceTest {
 	@Autowired
 	private TestEntityHelper helper;
 
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
+
 	private Town town;
 	private User user;
 	private Checklist checklist;
 	private UserChecklist userChecklist;
+
+	private final String CHECKLIST_PREFIX = "CC:";
 
 	@BeforeEach
 	void setUp() {
@@ -43,5 +54,23 @@ public class ChecklistServiceTest {
 	@Test
 	void 체크리스트조회() {
 		assertNotNull(checklistService.getUserChecklist(user));
+	}
+
+	@Test
+	void 커스텀체크리스트생성() {
+		CustomChecklistRequest request = new CustomChecklistRequest("형광등 전원 끄기");
+		checklistService.addCustomChecklist(user, request);
+		String userKey = CustomChecklistUtil.buildUserKey(user);
+		assertEquals(1, redisTemplate.opsForZSet().size(userKey));
+
+		Set<String> uuids = redisTemplate.opsForZSet().range(userKey, 0, -1);
+		assertNotNull(uuids);
+		assertEquals(1, uuids.size());
+
+		uuids.forEach(uuid -> {
+			Map<Object, Object> data = redisTemplate.opsForHash().entries(CHECKLIST_PREFIX + uuid);
+			String actualDescription = (String)data.get("description");
+			assertEquals("형광등 전원 끄기", actualDescription);
+		});
 	}
 }
