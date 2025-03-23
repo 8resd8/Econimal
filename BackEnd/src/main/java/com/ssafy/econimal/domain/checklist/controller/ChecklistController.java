dd3ac6a780069f24a96cf3b406b4fa098475f715
윤윤호 <yunho_yun@naver.com>
2025-03-24 00:03:10 +0900

    feat: 커스텀 체크리스트 수정 컨트롤러 구현

--- BackEnd/src/main/java/com/ssafy/econimal/domain/checklist/controller/ChecklistController.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/checklist/controller/ChecklistController.java
@@ -1,11 +1,14 @@
 package com.ssafy.econimal.domain.checklist.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.econimal.domain.checklist.dto.CustomChecklistRequest;
@@ -29,8 +32,15 @@ public class ChecklistController {
 	}
 
 	@PostMapping("/custom")
+	@ResponseStatus(HttpStatus.CREATED)
 	public void addCustomChecklist(@Login User user, @RequestBody CustomChecklistRequest request) {
 		checklistService.addCustomChecklist(user, request);
+	}
+
+	@PatchMapping("/custom/{checklistId}")
+	public void updateCustomChecklist(@Login User user, @RequestBody CustomChecklistRequest request,
+		@PathVariable("checklistId") String checklistId) {
+		checklistService.updateCustomChecklist(user, checklistId, request);
 	}
 
 	@DeleteMapping("/custom/{checklistId}")
