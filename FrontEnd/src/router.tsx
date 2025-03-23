5f1ee88560da6310e83faba749d45cffc48381e7
dabeen <dabin12648@gmail.com>
2025-03-24 00:05:34 +0900

    feat: 회원 정보 수정 기능 추가

--- FrontEnd/src/router.tsx
+++ FrontEnd/src/router.tsx
@@ -7,6 +7,7 @@ import Signup from './pages/Auth/SignUp';
 import MyPage from './pages/Auth/UserInfo';
 import Earth from './pages/earth/Earth';
 import Animation from './pages/animation/Animation';
+import Edit from './pages/Auth/InfoEdit'
 
 export const router = createBrowserRouter([
   {
@@ -40,5 +41,9 @@ export const router = createBrowserRouter([
   {
     path: '/animation',
     element: <Animation />
-  }
+  },
+  {
+    path: '/edit-profile',
+    element: <Edit />
+  },
 ]);
