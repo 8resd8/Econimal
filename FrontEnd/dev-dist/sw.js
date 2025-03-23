c77e576129d95d790a50fb75fdad3931ccc3022e
zoe <ddu_buk_e@naver.com>
2025-03-24 01:20:47 +0900

    feat: 오염 강물

--- FrontEnd/dev-dist/sw.js
+++ FrontEnd/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-78d1d747'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fqpm30t7qvg"
+    "revision": "0.vnhjasi41co"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
