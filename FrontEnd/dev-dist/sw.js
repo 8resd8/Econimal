664c9c760804571a7287e4f38bf870f7fd5f311c
JUMINAHN <jumin9774@naver.com>
2025-03-25 22:00:07 +0900

    feat: 새로운 배경 데이터 추가

--- FrontEnd/dev-dist/sw.js
+++ FrontEnd/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-78d1d747'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hoddsiopnpg"
+    "revision": "0.048j3u4b8r"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- FrontEnd/src/config/backgroundShopConfig.ts
+++ FrontEnd/src/config/backgroundShopConfig.ts
@@ -1,6 +1,9 @@
 import bugi from '../assets/char/background/tur_back.png';
 import peng from '../assets/char/background/peng_back.png';
 import horang from '../assets/char/background/horang_back.png';
+import bugiv2 from '../assets/char/background/bugi_seaver1.png';
+import pengv2 from '../assets/char/background/peng_icever1.png';
+import horangv2 from '../assets/char/background/horang_yeildver1.png';
 
 export const backgroundShopConfig = [
   {
@@ -23,5 +26,26 @@ export const backgroundShopConfig = [
     image: horang,
     price: 0,
     owned: true,
+  },
+  {
+    productId: 4,
+    characterName: '더 넓은 바다',
+    image: bugiv2,
+    price: 1000,
+    owned: false,
+  },
+  {
+    productId: 5,
+    characterName: '더 큰 빙하',
+    image: pengv2,
+    price: 1000,
+    owned: false,
+  },
+  {
+    productId: 6,
+    characterName: '더 넓은 초원',
+    image: horangv2,
+    price: 1000,
+    owned: false,
   },
 ];
