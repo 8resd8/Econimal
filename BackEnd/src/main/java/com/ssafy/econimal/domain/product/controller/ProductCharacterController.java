4c92030488ed91aa6dfbf27d7dce58afe24c08e7
김현우 <dkanfjgwls@naver.com>
2025-03-25 17:16:08 +0900

    feat: 상점구매 api 생성

--- BackEnd/src/main/java/com/ssafy/econimal/domain/product/controller/ProductCharacterController.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/product/controller/ProductCharacterController.java
@@ -1,7 +1,11 @@
 package com.ssafy.econimal.domain.product.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.econimal.domain.product.dto.ProductResponse;
@@ -14,13 +18,19 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/product")
 @RequiredArgsConstructor
-public class ProductController {
+public class ProductCharacterController {
 
 	private final ProductService productService;
 
 	@GetMapping("/characters")
 	public ProductResponse getCharacterProducts(@Login User user) {
 		return productService.getCharacterProducts(user);
+	}
+
+	@PostMapping("/{productId}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void buyCharacterProducts(@Login User user, @PathVariable("productId") Long productId) {
+		productService.buyCharacterProduct(user, productId);
 	}
 
 }
