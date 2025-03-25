6e9484690558b1cb23295dda428a10720ef7fdc5
김현우 <dkanfjgwls@naver.com>
2025-03-25 17:13:17 +0900

    feat: 상점 구매 로직추가

--- BackEnd/src/main/java/com/ssafy/econimal/domain/product/service/ProductService.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/product/service/ProductService.java
@@ -5,10 +5,17 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.econimal.domain.character.entity.Character;
 import com.ssafy.econimal.domain.product.dto.ProductDto;
 import com.ssafy.econimal.domain.product.dto.ProductResponse;
+import com.ssafy.econimal.domain.product.entity.Product;
 import com.ssafy.econimal.domain.product.repository.ProductCharacterQueryRepository;
+import com.ssafy.econimal.domain.product.repository.ProductRepository;
 import com.ssafy.econimal.domain.user.entity.User;
+import com.ssafy.econimal.domain.user.entity.UserCharacter;
+import com.ssafy.econimal.domain.user.repository.UserCharacterRepository;
+import com.ssafy.econimal.global.common.enums.ExpressionType;
+import com.ssafy.econimal.global.exception.InvalidArgumentException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,9 +25,45 @@ import lombok.RequiredArgsConstructor;
 public class ProductService {
 
 	private final ProductCharacterQueryRepository productQueryRepository;
+	private final ProductRepository productRepository;
+	private final UserCharacterRepository userCharacterRepository;
 
 	public ProductResponse getCharacterProducts(User user) {
 		List<ProductDto> products = productQueryRepository.findAllCharactersStore(user);
 		return new ProductResponse(products);
+	}
+
+	public void buyCharacterProduct(User user, Long productId) {
+		Product wantProductItem = findProductById(productId);
+		validateUserCoin(user, wantProductItem.getPrice());
+
+		UserCharacter userCharacter = createUserCharacter(user, wantProductItem.getCharacter());
+		userCharacterRepository.save(userCharacter);
+
+		updateUserCoin(user, wantProductItem.getPrice());
+	}
+
+	private Product findProductById(Long productId) {
+		return productRepository.findById(productId)
+			.orElseThrow(() -> new IllegalArgumentException("해당 상품을 찾을 수 없습니다."));
+	}
+
+	private void validateUserCoin(User user, long productPrice) {
+		if (user.getCoin() < productPrice) {
+			throw new InvalidArgumentException("보유한 코인이 부족합니다.");
+		}
+	}
+
+	private UserCharacter createUserCharacter(User user, Character character) {
+		return UserCharacter.builder()
+			.user(user)
+			.character(character)
+			.expression(ExpressionType.SADNESS)
+			.isMain(false)
+			.build();
+	}
+
+	private void updateUserCoin(User user, long productPrice) {
+		user.updateCoin(user.getCoin() - productPrice);
 	}
 }
