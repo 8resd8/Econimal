4c48c7b69395c2e2f10230b3fd5f3f6c25dd516b
김현우 <dkanfjgwls@naver.com>
2025-03-26 15:11:24 +0900

    feat: 유저로 유저배경 조회 쿼리

--- BackEnd/src/main/java/com/ssafy/econimal/domain/user/repository/UserBackgroundRepository.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/user/repository/UserBackgroundRepository.java
@@ -1,11 +1,12 @@
 package com.ssafy.econimal.domain.user.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.ssafy.econimal.domain.product.entity.Product;
 import com.ssafy.econimal.domain.user.entity.User;
 import com.ssafy.econimal.domain.user.entity.UserBackground;
 
 public interface UserBackgroundRepository extends JpaRepository<UserBackground, Long> {
-	boolean existsByUserAndProduct(User user, Product product);
+	List<UserBackground> findByUser(User user);
 }
