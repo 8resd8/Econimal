d1171a004580268a4e5583c7f301c35659b439b2
김현우 <dkanfjgwls@naver.com>
2025-03-26 14:12:13 +0900

    test: 유저 배경추가

--- BackEnd/src/test/java/com/ssafy/econimal/insertdata/InsertProduct.java
+++ BackEnd/src/test/java/com/ssafy/econimal/insertdata/InsertProduct.java
@@ -7,6 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.ssafy.econimal.domain.product.entity.Product;
 import com.ssafy.econimal.domain.product.repository.ProductRepository;
+import com.ssafy.econimal.domain.user.entity.User;
+import com.ssafy.econimal.domain.user.entity.UserBackground;
+import com.ssafy.econimal.domain.user.repository.UserBackgroundRepository;
+import com.ssafy.econimal.domain.user.repository.UserRepository;
 import com.ssafy.econimal.global.common.enums.ProductType;
 
 @SpringBootTest
@@ -14,6 +18,15 @@ public class InsertProduct {
 
 	@Autowired
 	private ProductRepository productRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private UserBackgroundRepository backgroundRepository;
+
+	@Autowired
+	private UserBackgroundRepository userBackgroundRepository;
 
 	@Test
 	@Disabled
@@ -28,6 +41,19 @@ public class InsertProduct {
 			productRepository.save(background);
 		}
 
+	}
+
+	@Test
+	@Disabled
+	void 유저배경추가() {
+		User user = userRepository.findById(12L).get();
+		Product product = productRepository.findById(1779L).get();
+		UserBackground userBackground = UserBackground.builder()
+			.product(product)
+			.user(user)
+			.isMain(true)
+			.build();
+		userBackgroundRepository.save(userBackground);
 	}
 
 }
