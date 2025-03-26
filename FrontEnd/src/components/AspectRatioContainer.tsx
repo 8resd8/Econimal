7927eb86198b4b297277512e5b14744aa0c672fa
Dabeen <dabin12648@gmail.com>
2025-03-26 17:49:38 +0900

    fix: 모바일 화면 비율에 맞춘 scale 설정

--- FrontEnd/src/components/AspectRatioContainer.tsx
+++ FrontEnd/src/components/AspectRatioContainer.tsx
@@ -7,10 +7,10 @@ interface AspectRatioProps {
 const AspectRatioContainer = ({ children }: AspectRatioProps) => {
   return (
     <div className='fixed inset-0 flex items-center justify-center bg-black'>
-      <div className='relative w-full max-w-screen-2xl aspect-video bg-white overflow-hidden'>
+      {/* <div className='relative w-full max-w-screen-2xl aspect-video bg-white overflow-hidden'> */}
         {/* <div className='relative w-full max-w-screen-2xl aspect-[20/9] bg-white overflow-hidden'> */}
         {children}
-      </div>
+      {/* </div> */}
     </div>
   );
 };
