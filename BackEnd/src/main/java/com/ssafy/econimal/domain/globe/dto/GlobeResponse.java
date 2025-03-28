c41779ac88efb5b23a6ecd8159669c770bbc5d5f
주재완 <jjw05015@naver.com>
2025-03-28 14:53:24 +0900

    feat: 지구촌 - Response Type 변경

--- BackEnd/src/main/java/com/ssafy/econimal/domain/globe/dto/GlobeResponse.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/globe/dto/GlobeResponse.java
@@ -1,8 +1,7 @@
 package com.ssafy.econimal.domain.globe.dto;
 
-import java.util.List;
-
 public record GlobeResponse(
-	List<GlobeInfoDto> globeInfoDto
+	GroupByDateTimeDto groupByDateTime,
+	GroupByCountryDto groupByCountry
 ) {
 }
