187b7ebf9c5ee01c6a17f7f7f181e61291d80aa1
Dabeen <dabin12648@gmail.com>
2025-03-28 10:14:11 +0900

    fix: 충돌 해결

--- FrontEnd/dev-dist/sw.js
+++ FrontEnd/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-78d1d747'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v42dd3r839"
+    "revision": "0.dgkn5j0qfa8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
