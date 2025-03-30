74ad05454c3758bed92a51d2934b63752efc8b8a
JUMINAHN <jumin9774@naver.com>
2025-03-31 00:59:16 +0900

    design: 체크리스트 표시 직관적으로 바꾸기 위한 design 수정

--- FrontEnd/src/pages/character/feature/status/CharMenu.tsx
+++ FrontEnd/src/pages/character/feature/status/CharMenu.tsx
@@ -23,13 +23,23 @@ export function CharMenu() {
         onClick={handleOpenChecklist}
       >
         <svg
+          xmlns='http://www.w3.org/2000/svg'
           width='24'
           height='24'
+          viewBox='0 0 24 24'
           fill='none'
           stroke='currentColor'
           strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          className='w-6 h-6 text-blue-700'
         >
-          <path d='M4 6h16M4 12h16M4 18h16' />
+          <path d='M3.5 5.5l1.5 1.5l2.5 -2.5' />
+          <path d='M3.5 11.5l1.5 1.5l2.5 -2.5' />
+          <path d='M3.5 17.5l1.5 1.5l2.5 -2.5' />
+          <path d='M11 6h9' />
+          <path d='M11 12h9' />
+          <path d='M11 18h9' />
         </svg>
       </button>
 
