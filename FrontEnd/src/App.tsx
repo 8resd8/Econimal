2132b1b9f28d46623da6abb6274d91d05ffc122f
Zoe <ddu_buk_e@naver.com>
2025-03-31 17:26:50 +0900

    feat: 토스트 호버 및 드래그 반응 변경, 로그아웃 시 토스트 제거, 결과 모달 UI 수정

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -4,8 +4,8 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { router } from './router';
 import { RouterProvider } from 'react-router-dom';
 import AspectRatioContainer from '@/components/AspectRatioContainer';
-// import EventDetector from './components/EventDetector';
 import { ToastContainer } from 'react-toastify';
+import { TOAST_CONTAINER_ID } from './components/toast/toastUtil';
 
 const queryClient = new QueryClient(); // 일단 기본 옵션으로 설정. 추가 옵션 설정 후 파일 분리 및 import 해서 사용해도 됨
 // 쿼리 클라이언트 : 쿼리, 캐시, 쿼리 캐시를 조작하는 도구가 속함.
@@ -16,10 +16,14 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <AspectRatioContainer>
         <RouterProvider router={router} />
-        {/* RouterProvider만 렌더링하고 EventDetector는 라우터 내부에서 처리 */}
-        {/* 전역 이벤트 감지기 */}
-        {/* <EventDetector /> */}
       </AspectRatioContainer>
+
+      {/* 토스트 컨테이너에 ID 추가 및 옵션 명시적 설정 */}
+      <ToastContainer
+        containerId={TOAST_CONTAINER_ID}
+        draggable={false}
+        pauseOnHover={false}
+      />
 
       <ToastContainer />
 
--- FrontEnd/src/components/RootLayout.tsx
+++ FrontEnd/src/components/RootLayout.tsx
@@ -1,8 +1,19 @@
-import { Outlet } from 'react-router-dom';
-import EventDetector from '../components/EventDetector';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import EventDetector from '@/components/EventDetector';
+import { clearAllToasts } from './toast/toastUtil';
 
 // 모든 라우트의 루트 레이아웃 컴포넌트
 const RootLayout = () => {
+  const location = useLocation();
+
+  // 로그인 페이지로 이동 시 토스트 제거
+  useEffect(() => {
+    if (location.pathname === '/login') {
+      clearAllToasts();
+    }
+  }, [location]);
+
   return (
     <>
       <EventDetector />
