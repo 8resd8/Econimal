21748cc32f91409a0066d1fa963a0d83ee9c36e5
Zoe <ddu_buk_e@naver.com>
2025-03-31 14:34:38 +0900

    feat: 라우터 구조 변경

--- FrontEnd/dev-dist/sw.js
+++ FrontEnd/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-78d1d747'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vgiuu5512so"
+    "revision": "0.e4jq24te3m8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
