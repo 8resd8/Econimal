5530adbe366cda4132c408439b88b4058d23b103
DESKTOP-4QCTMKG\SSAFY <jumin9774@naver.com>
2025-04-03 17:04:49 +0900

    fix: 내부 QA_상점 화면 너비 비율 설정

--- FrontEnd/src/pages/character/componet/shop/ItemShopUI.tsx
+++ FrontEnd/src/pages/character/componet/shop/ItemShopUI.tsx
@@ -38,11 +38,6 @@ const ItemShopUI = ({
     const selectedItem = currentItems.find(
       (item) => item.productId === productId,
     );
-    console.log(
-      productId,
-      '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',
-      '선택을 누르면 같이 발생되는게 맞는거가? => 현재 선택을 누르면 호버가 처음에 발생',
-    );
 
     if (selectedItem) {
       if (selectedTab === 'characters' && selectedItem.userCharacterId) {
@@ -57,10 +52,10 @@ const ItemShopUI = ({
   };
 
   return (
-    <div className='w-screen h-screen bg-black p-2 flex flex-col items-center relative'>
-      <div className='w-full max-w-[812px] flex flex-col items-center h-full'>
+    <div className='w-screen h-screen bg-black p-2 flex flex-col items-center relative pt-6 '>
+      <div className='w-full max-w-[812px] flex flex-col items-center h-full px-4'>
         {/* 상점 제목 & 코인 표시 */}
-        <div className='flex items-center justify-between w-full px-2 mb-2 relative'>
+        <div className='flex items-center justify-between w-full px-2 mb-2 relative pb-2 '>
           <GoMainBtn />
           <h1 className='absolute left-1/2 transform -translate-x-1/2 text-3xl font-bold text-white'>
             상점
@@ -85,8 +80,8 @@ const ItemShopUI = ({
         </div>
 
         {/* 아이템 리스트 */}
-        <div className='flex-grow w-full overflow-y-auto'>
-          <div className='grid grid-cols-4 gap-1 w-full'>
+        <div className='flex w-full overflow-y-auto justify-center'>
+          <div className='grid grid-cols-4 gap-3 w-[93%] pt-6'>
             {currentItems &&
               currentItems.map((item: any, index: number) => (
                 <ItemShopItems
