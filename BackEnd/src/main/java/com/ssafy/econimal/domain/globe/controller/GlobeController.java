0066f78b6fcc03d31dfb55f1fd2c0200c9cf74d7
김현우 <dkanfjgwls@naver.com>
2025-04-04 13:48:08 +0900

    feat: v2 기후조회

--- BackEnd/src/main/java/com/ssafy/econimal/domain/globe/controller/GlobeController.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/globe/controller/GlobeController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.econimal.domain.globe.dto.GlobeInfoRequest;
 import com.ssafy.econimal.domain.globe.dto.GlobeResponse;
+import com.ssafy.econimal.domain.globe.dto.GlobeV2Response;
 import com.ssafy.econimal.domain.globe.service.GlobeService;
+import com.ssafy.econimal.global.common.enums.TimeType;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,8 +30,14 @@ public class GlobeController {
 		return globeService.getGlobeInfoByRDB(globeInfoRequest);
 	}
 
+	/**
+	 * 72시간 3일
+	 * 3달
+	 * 1년
+	 * @param type 원하는 통계 시간
+	 */
 	@GetMapping("/v2")
-	public GlobeResponse getGlobeInfos(@Valid @ModelAttribute GlobeInfoRequest request) {
-		return globeService.getGlobeInfoByRDBV2(request);
+	public GlobeV2Response getGlobeInfos(@Valid @RequestParam(name = "timeType") TimeType type) {
+		return globeService.getGlobeInfoByRDBV2(type);
 	}
 }
