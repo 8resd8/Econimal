6ed7a163a95ad6de067feb1157416fa72f3735db
JUMINAHN <jumin9774@naver.com>
2025-04-05 23:51:54 +0900

    design: 250405_2351 상점 선택 UI 변경

--- FrontEnd/dev-dist/sw.js
+++ FrontEnd/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-78d1d747'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vo4abf6qkj8"
+    "revision": "0.49o3v00fg1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
