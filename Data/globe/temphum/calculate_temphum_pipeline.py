97eceb2ecd6eb8bb0b1b49b99f343117f0885ec5
윤윤호 <yunho_yun@naver.com>
2025-04-05 12:25:26 +0900

    fix: 온습도 평균을 연 단위로 제공하도록 변경

--- Data/globe/temphum/calculate_temphum_pipeline.py
+++ Data/globe/temphum/calculate_temphum_pipeline.py
@@ -12,10 +12,10 @@ import redis
 first_time = time.time()
 
 spark = SparkSession.builder \
-            .appName("CalculateTempHum") \
+            .appName("CalculateTempHumPipeline") \
             .master("spark://spark-master:7077") \
             .config("spark.hadoop.fs.defaultFS", "hdfs://namenode:9000") \
-            .config("spark.redis.host", "YOUR_REDIS_HOST") \
+            .config("spark.redis.host", "43.203.120.223") \
             .config("spark.redis.port", "6379") \
             .config("spark.redis.pipeline.enabled", "true") \
             .config("spark.redis.write.batch.size", "1000")  \
@@ -32,12 +32,31 @@ spark.conf.set("spark.sql.parquet.mergeSchema", "false")
 
 # parquet 로드
 print("[Start] Load Parquet")
-df = spark.read.parquet("/climate/temphum/collect/parquet")
+df_parquet = spark.read.parquet("/climate/temphum/collect/parquet")
 print("[Finish] Load Parquet")
 
-# timestamp로 변경 및 월 단위로
-df = df.withColumn("reference_date", F.col("reference_date").cast("timestamp"))
-df = df.withColumn("reference_date", F.date_trunc("month", F.col("reference_date")))
+# csv 로드
+print("[Start] Load CSV")
+df_csv = spark.read.option("header", "true").csv("/climate/temphum/collect/csv")
+print("[Finish] Load CSV")
+
+# reference_date timestamp 변환 후 월 단위로 자르기
+print("[CAST] 공통 컬럼 캐스팅 및 정제")
+df_csv = df_csv.withColumn("reference_date", F.to_timestamp("reference_date", "yyyy-MM-dd HH:mm:ss"))
+df_parquet = df_parquet.withColumn("reference_date", F.col("reference_date").cast("timestamp"))
+
+# 스키마 맞추기
+common_columns = [
+    "country_code",
+    F.col("year"),
+    F.col("temperature").cast("double").alias("temperature"),
+    F.col("humidity").cast("double").alias("humidity")
+]
+
+df_csv_clean = df_csv.select(*common_columns)
+df_parquet_clean = df_parquet.select(*common_columns)
+
+df = df_csv_clean.unionByName(df_parquet_clean)
 
 # 필요한 파티션 수로 조정
 print("[Before] 파티션 수:", df.rdd.getNumPartitions())
@@ -47,7 +66,7 @@ print("[After] 파티션 수:", df.rdd.getNumPartitions())
 # 전체 기간 월 평균
 print("[Agg] 온습도 평균 계산 시작")
 start_time = time.time()
-monthly_avg_all = df.groupBy("country_code", "reference_date") \
+monthly_avg_all = df.groupBy("country_code", "year") \
                     .agg(
                         F.avg("temperature").alias("avg_temperature"),
                         F.avg("humidity").alias("avg_humidity")
@@ -56,7 +75,7 @@ print("[Agg] 온습도 평균 계산 시간: {:.2f}초".format(time.time() - sta
 
 print("[Json] Json 생성 시작")
 start_time = time.time()
-df_groupByDateTime = monthly_avg_all.groupBy("reference_date").agg(
+df_groupByDateTime = monthly_avg_all.groupBy("year").agg(
     F.map_from_entries(
         F.collect_list(
             F.struct(
@@ -71,13 +90,13 @@ df_groupByDateTime = monthly_avg_all.groupBy("reference_date").agg(
 )
 
 df_groupByDateTime = df_groupByDateTime.select(
-    F.date_format("reference_date", "yyyy-MM-dd HH:mm:ss").alias("date"),
+    F.col("year"),
     F.col("country_map")
 )
 
 df_groupByDateTime_map = df_groupByDateTime.select(
     F.struct(
-        F.col("date").alias("key"),
+        F.col("year").alias("key"),
         F.col("country_map").alias("value")
     ).alias("entry")
 ).agg(
@@ -88,7 +107,7 @@ df_groupByCountry = monthly_avg_all.groupBy("country_code").agg(
     F.map_from_entries(
         F.collect_list(
             F.struct(
-                F.date_format("reference_date", "yyyy-MM-dd HH:mm:ss").alias("key"),
+                F.col("year").alias("key"),
                 F.struct(
                     F.col("avg_temperature").cast("string").alias("temperature"),
                     F.col("avg_humidity").cast("string").alias("humidity")
@@ -120,12 +139,12 @@ print("[Redis] Redis 저장용 데이터 준비 시작")
 start_time = time.time()
 flat_df = monthly_avg_all.select(
     F.col("country_code"),
-    F.date_format("reference_date", "yyyy-MM-dd HH:mm:ss").alias("date"),
+    F.col("year"),
     F.col("avg_temperature").cast("string"),
     F.col("avg_humidity").cast("string")
 )
 kv_df = flat_df.select(
-    F.concat_ws(":", F.lit("climate"), "country_code", "date").alias("key"),
+    F.concat_ws(":", F.lit("climate"), "country_code", "year").alias("key"),
     F.col("avg_temperature"),
     F.col("avg_humidity")
 )
@@ -135,7 +154,7 @@ print("[All] Redis 저장용 데이터 준비 시간: {:.2f}초".format(time.tim
 
 print("[Redis] Redis 저장 시작")
 start_time = time.time()
-redis_client = redis.StrictRedis(host='YOUR_REDIS_HOST', port=6379, db=0, decode_responses=True)
+redis_client = redis.StrictRedis(host='43.203.120.223', port=6379, db=0, decode_responses=True)
 pipe = redis_client.pipeline()
 
 for row in kv_list:
