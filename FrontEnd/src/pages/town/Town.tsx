b77ea902e7e14a5a37098a309d793c2b6349f452
dabeen <dabin12648@gmail.com>
2025-04-07 02:55:42 +0900

    fix: 마을 전체화면으로 변경

--- FrontEnd/src/pages/town/Town.tsx
+++ FrontEnd/src/pages/town/Town.tsx
@@ -42,66 +42,54 @@ const TownContent = ({ data }: TownContentProps) => {
   };
 
   return (
-    // 전체 화면을 차지하는 고정 컨테이너
+    // 전체 화면을 차지하는 컨테이너
     <div className='fixed inset-0 overflow-hidden'>
-      <div className='absolute inset-0'>
+      {/* 배경 이미지 - 전체 화면 채우기 */}
+      <div className='absolute inset-0 w-full h-full'>
         <img
           src={town}
-          alt='뒷 배경'
-          className='w-full h-full object-cover blur-3xl'
+          alt='마을'
+          className='w-full h-full object-cover'
+          loading='eager'
         />
-      </div>
-      {/* 배경 이미지 래퍼 - 배경 이미지를 화면 중앙에 배치 */}
-      <div className='relative w-full h-full flex items-center justify-center'>
-        {/* 배경 이미지 - 비율 유지하면서 화면에 맞춤 */}
-        <div className='relative max-w-full max-h-screen'>
+        
+        {/* 오염된 강물 오버레이 - 하수처리장이 오염 상태일 때만 표시 */}
+        {!infraStatus.WATER && (
           <img
-            src={town}
-            alt='마을'
-            className='max-w-full max-h-screen object-contain filter saturate-[.75]'
-            loading='eager' // [여기] 이미지 우선 로딩
+            src={pollutedImg}
+            alt='오염된 강물'
+            className='absolute inset-0 w-full h-full object-cover z-10 pointer-events-none'
           />
+        )}
 
-          {/* 컴포넌트 배치를 위한 절대 위치 오버레이 (이미지와 정확히 동일한 위치와 크기) */}
-          <div className='absolute inset-0'>
-            <div className=''>
-              {/* 홈으로 가는 버튼 */}
-              <div className='absolute top-[5%] left-[3%] w-[20%] h-[20%] z-30'>
-                <GoMainBtn />
-              </div>
+        {/* 홈으로 가는 버튼 */}
+        <div className='absolute top-[5%] left-[3%] w-[10%] z-30'>
+          <GoMainBtn />
+        </div>
 
-              {/* 마을 이름 - 항상 상단 중앙에 위치 */}
-              <div className='absolute top-[6%] left-[50%] transform -translate-x-1/2 w-[20%] h-[5%] z-30'>
-                <TownName />
-              </div>
-            </div>
+        {/* 마을 이름 - 항상 상단 중앙에 위치 */}
+        <div className='absolute top-[6%] left-[50%] transform -translate-x-1/2 w-[20%] z-30'>
+          <TownName />
+        </div>
 
-            {/* 가정 컴포넌트 - 배경 이미지 기준 상대적 위치 */}
-            <div className='absolute top-[58%] left-[86%] transform -translate-x-1/2 -translate-y-1/2 w-[15%] z-20'>
-              <MyHouse infraEventId={getInfraEventId('ELECTRICITY')} />
-            </div>
+        {/* 가정 컴포넌트 - 화면 비율에 맞게 상대적 위치 */}
+        <div className='absolute top-[58%] left-[86%] transform -translate-x-1/2 -translate-y-1/2 w-[15%] z-20'>
+          <MyHouse infraEventId={getInfraEventId('ELECTRICITY')} />
+        </div>
 
-            {/* 하수처리장 컴포넌트 - 배경 이미지 기준 상대적 위치 */}
-            <div className='absolute top-[51%] left-[45%] transform -translate-x-1/2 -translate-y-1/2 w-[13%] z-20'>
-              <SewageTreatmentCenter infraEventId={getInfraEventId('WATER')} />
-            </div>
-            {/* 오염된 강물 오버레이 - 하수처리장이 오염 상태일 때만 표시 */}
-            {!infraStatus.WATER && (
-              <img
-                src={pollutedImg}
-                alt='오염된 강물'
-                className='absolute top-0 left-0 max-w-full max-h-screen object-contain z-10 pointer-events-none'
-              />
-            )}
-            {/* 공장 */}
-            <div className='absolute top-[30%] left-[78%] transform -translate-x-1/2 -translate-y-1/2 w-[20%] z-20'>
-              <Factory infraEventId={getInfraEventId('GAS')} />
-            </div>
-            {/* 법원 컴포넌트 - 배경 이미지 기준 상대적 위치 */}
-            <div className='absolute top-[84%] left-[19%] transform -translate-x-1/2 -translate-y-1/2 w-[14%] z-20'>
-              <Court infraEventId={getInfraEventId('COURT')} />
-            </div>
-          </div>
+        {/* 하수처리장 컴포넌트 */}
+        <div className='absolute top-[51%] left-[45%] transform -translate-x-1/2 -translate-y-1/2 w-[13%] z-20'>
+          <SewageTreatmentCenter infraEventId={getInfraEventId('WATER')} />
+        </div>
+        
+        {/* 공장 */}
+        <div className='absolute top-[30%] left-[78%] transform -translate-x-1/2 -translate-y-1/2 w-[20%] z-20'>
+          <Factory infraEventId={getInfraEventId('GAS')} />
+        </div>
+        
+        {/* 법원 컴포넌트 */}
+        <div className='absolute top-[84%] left-[19%] transform -translate-x-1/2 -translate-y-1/2 w-[14%] z-20'>
+          <Court infraEventId={getInfraEventId('COURT')} />
         </div>
       </div>
     </div>
