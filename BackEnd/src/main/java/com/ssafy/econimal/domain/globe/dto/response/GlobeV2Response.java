7e35f3ed7440883030fba057d1b284cbe64bdca1
김현우 <dkanfjgwls@naver.com>
2025-04-06 23:05:56 +0900

    refactor: 변수명 뒤 Time 추가

--- BackEnd/src/main/java/com/ssafy/econimal/domain/globe/dto/response/GlobeV2Response.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/globe/dto/response/GlobeV2Response.java
@@ -4,6 +4,6 @@ import java.util.Map;
 
 public record GlobeV2Response(
 	Map<String, Map<String, Map<String, String>>> groupByCountry,
-	Map<String, Map<String, Map<String, String>>> groupByDate
+	Map<String, Map<String, Map<String, String>>> groupByDateTime
 ) {
 }
