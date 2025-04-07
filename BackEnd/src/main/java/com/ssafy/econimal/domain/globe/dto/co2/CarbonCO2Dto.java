8866e8e1baf027d56efbeb2bbcbbc52103ae3628
윤윤호 <yunho_yun@naver.com>
2025-04-07 10:09:47 +0900

    feat: CO2 interface 구현

--- BackEnd/src/main/java/com/ssafy/econimal/domain/globe/dto/co2/CarbonCO2Dto.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/globe/dto/co2/CarbonCO2Dto.java
@@ -1,13 +1,23 @@
 package com.ssafy.econimal.domain.globe.dto.co2;
 
+import java.util.Map;
+
 import com.querydsl.core.annotations.QueryProjection;
+import com.ssafy.econimal.domain.globe.dto.GlobeData;
 
 public record CarbonCO2Dto(
 	String country,
 	String formattedDateHour,
 	Double co2
-) {
+) implements GlobeData {
 	@QueryProjection
 	public CarbonCO2Dto {
+	}
+
+	@Override
+	public Map<String, String> toValueMap() {
+		return Map.of(
+			"co2", String.valueOf(this.co2())
+		);
 	}
 }
