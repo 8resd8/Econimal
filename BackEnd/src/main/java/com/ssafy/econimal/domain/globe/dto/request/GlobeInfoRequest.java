4b5976a94683939fb15418d8f1e82bf8f988e482
김현우 <dkanfjgwls@naver.com>
2025-04-08 17:28:42 +0900

    fix: 기후조회 요청값 LocalDateTime 형식에 맞는 검증 추가

--- BackEnd/src/main/java/com/ssafy/econimal/domain/globe/dto/request/GlobeInfoRequest.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/globe/dto/request/GlobeInfoRequest.java
@@ -2,6 +2,7 @@ package com.ssafy.econimal.domain.globe.dto.request;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ssafy.econimal.global.annotation.EnumValid;
 import com.ssafy.econimal.global.common.enums.TimeType;
 
@@ -11,10 +12,12 @@ import jakarta.validation.constraints.PastOrPresent;
 public record GlobeInfoRequest(
 
 	@NotNull(message = "{required}")
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
 	LocalDateTime startDate,
 
 	@NotNull(message = "{required}")
 	@PastOrPresent(message = "{past}")
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
 	LocalDateTime endDate,
 
 	@NotNull(message = "{required}")
