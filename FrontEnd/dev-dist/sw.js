e78ac5c7dbb04b7b0153182f7bc4fcc5e7ed0b10
Dabeen <dabin12648@gmail.com>
2025-04-08 12:30:29 +0900

    fix: 지역 선택시 이전 지역 차트 보이는 에러 해결

--- FrontEnd/dev-dist/sw.js
+++ FrontEnd/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-78d1d747'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gddib4k07ho"
+    "revision": "0.plbs5djvft"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
