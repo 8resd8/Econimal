d5f64315754dd70837b83cce087059d784e40f87
Dabeen <dabin12648@gmail.com>
2025-04-08 17:17:40 +0900

    feat: 온습도 연도별 api 분리

--- FrontEnd/dev-dist/sw.js
+++ FrontEnd/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-78d1d747'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.p6j7r74kk9o"
+    "revision": "0.f2e0q5bopl"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
