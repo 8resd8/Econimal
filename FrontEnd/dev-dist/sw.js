3462ecf5944767890bd76759ec9dd754df512bfc
Dabeen <dabin12648@gmail.com>
2025-04-09 10:57:50 +0900

    design: 홈버튼 수정

--- FrontEnd/dev-dist/sw.js
+++ FrontEnd/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-78d1d747'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.frsd4gqfjk"
+    "revision": "0.all5rp42hh8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
