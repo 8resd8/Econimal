9facfc1ee1ebbe0de8908fe3413c51b03a428bc2
DESKTOP-4QCTMKG\SSAFY <jumin9774@naver.com>
2025-04-09 18:25:12 +0900

    fix: 전체 내용 전반 수정 최종

--- FrontEnd/dev-dist/sw.js
+++ FrontEnd/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-78d1d747'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s7s41ckpua"
+    "revision": "0.jq0efbk27go"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
