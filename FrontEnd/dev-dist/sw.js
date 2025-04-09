7a5eddfd16781afd24507b945ba3694ca28eba7c
Zoe <ddu_buk_e@naver.com>
2025-04-09 15:33:11 +0900

    feat: 400에러 토스트 로그인 페이지 예외 처리

--- FrontEnd/dev-dist/sw.js
+++ FrontEnd/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-78d1d747'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qjf46857e7"
+    "revision": "0.bgmtf0leg8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
