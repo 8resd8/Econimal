d9cd8665d8ea2ac6e4f13f1edde7955d8bc616f6
주재완 <jjw05015@naver.com>
2025-04-10 10:18:11 +0900

    feat: 탄소 수치 변경

--- BackEnd/src/main/java/com/ssafy/econimal/domain/town/util/CarbonCalculator.java
+++ BackEnd/src/main/java/com/ssafy/econimal/domain/town/util/CarbonCalculator.java
@@ -4,19 +4,29 @@ import com.ssafy.econimal.global.common.enums.ExpressionType;
 
 public class CarbonCalculator {
 
-	public static final double CARBON_PERCENT_TO_EXP_RATIO = 25 / 1312.9846;
+	public static final double EXP_TO_CARBON_PERCENT_RATIO = -1.0 / 25;
 
-	private static final int ROUNDING_SCALE = 10;
+	public static final double EXP_TO_REAL_CARBON_RATIO = -1312.9846 / 25;
+
+	private static final int ROUNDING_SCALE = 1000;
 
 	private CarbonCalculator() {
 	}
 
-	public static double calculateCarbon(int exp) {
-		return -Math.round(convertExpToPercent(exp) * ROUNDING_SCALE) / (double)ROUNDING_SCALE;
+	public static double calculateCarbonPercent(int exp) {
+		return Math.round(convertExpToPercent(exp) * ROUNDING_SCALE) / (double)ROUNDING_SCALE;
+	}
+
+	public static double calculateRealCarbon(int exp) {
+		return Math.round(convertExpToRealCarbon(exp) * ROUNDING_SCALE) / (double)ROUNDING_SCALE;
 	}
 
 	public static double convertExpToPercent(int exp) {
-		return exp / CARBON_PERCENT_TO_EXP_RATIO;
+		return exp * EXP_TO_CARBON_PERCENT_RATIO;
+	}
+
+	public static double convertExpToRealCarbon(int exp) {
+		return exp * EXP_TO_REAL_CARBON_RATIO;
 	}
 
 	public static ExpFeedback evaluateExp(int rawExp) {
