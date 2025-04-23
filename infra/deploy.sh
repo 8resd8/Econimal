2d0a34289a44f3c405e5ff7d78b45c340b1ccd99
김현우 <dkanfjgwls@naver.com>
2025-04-23 09:28:51 +0900

    chore: cd 설정 테스트10

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
+set -e
 set -a; source .env; set +a
 
-docker start mysql redis
+echo "Starting MySQL and Redis (if not already running)..."
+sudo docker start mysql redis
+echo "MySQL and Redis are up."
+echo "-----------------------------------"
+
+echo "Pulling latest Docker images..."
+sudo docker compose pull
+echo "Docker images pulled."
+echo "-----------------------------------"
 
 EXIST_BLUE=$(docker compose ps | grep "backend-blue" | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
-    docker compose up -d backend-blue --build
+    echo "Blue backend is not running, deploying blue..."
+
+#    docker compose up -d backend-blue --build
+    docker compose up -d backend-blue --wait
     BEFORE_COLOR="green"
     AFTER_COLOR="blue"
     BEFORE_PORT=${GREEN_PORT}
     AFTER_PORT=${BLUE_PORT}
 else
-    docker compose up -d backend-green --build
+    echo "Blue backend is running, deploying green..."
+
+#    docker compose up -d backend-green --build
+    docker compose up -d backend-green --wait
     BEFORE_COLOR="blue"
     AFTER_COLOR="green"
     BEFORE_PORT=${BLUE_PORT}
